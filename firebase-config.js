345b8da7d2c136e57b8965d0b82a00a771000b59
ahmad nahid fatki <122498809+2511060@users.noreply.github.com>
2026-01-08 15:07:56 +0800

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-//firebase
-const firebaseConfig = {
-  apiKey: "Unspecified",
-  authDomain: "https://2511060.github.io/ahmad_nahid_fatki/login.html",
-  projectId: "toko-online-nahid2511060",
-  storageBucket: "toko-online-12345.appspot.com",
-  messagingSenderId: "111139481910",
-  appId: "1:111139481910:web:da8a57ddeef0f090c52b97"
-};
